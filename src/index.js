5d93d2968c79b90ec7fe32a04e554111f3248a09
Mitch Robb <olordhux+github@gmail.com>
2016-03-27 01:17:35 -0700

    add material ui  and tap event handling

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {render} from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
