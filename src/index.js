88ae0d06a8ae098ed78af1267fd376b163387773
Mitch Robb <olordhux+github@gmail.com>
2016-03-27 01:17:41 -0700

    formatting

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import configureStore from './store/configureStore';
+
 import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 import './styles/simple-grid.css'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
 
 const store = configureStore();
 
 render(
-  <Provider store={store}>
-    <Router history={browserHistory} routes={routes} />
+  <Provider store={ store }>
+    <Router history={ browserHistory } routes={ routes } />
   </Provider>, document.getElementById('app')
 );
